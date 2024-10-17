347c5722196fad2518b0103229015f1421097e11
Lucas Pires <lcpires7.dev@gmail.com>
2024-10-17 12:23:23 -0300

    estilizando a sidebar

--- src/containers/SideBar/index.tsx
+++ src/containers/SideBar/index.tsx
@@ -1,9 +1,20 @@
+import Avatar from '../../components/Avavatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { BotaoTema, Descricao, SideBarContainer } from './styless'
 
 const SideBar = () => (
   <aside>
-    <img src="http://github.com/LucasPires-dev.png" />
-    <Titulo fontSize={20}>Lucas C. Pires</Titulo>
+    <SideBarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Lucas C. Pires</Titulo>
+      <Paragrafo tipo="secundario">LucasPires-dev</Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SideBarContainer>
   </aside>
 )
 
---
+++ src/containers/SideBar/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+import { P } from '../../components/Paragrafo/styles'
+
+export const Descricao = styled(P)
+
+export const SideBarContainer = styled.div`
+  position: sticky;
+  top: 0;
+  left: 0;
+`
